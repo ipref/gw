98b675a17cff90582b5fb99b8f31026a897161c3
waldemar <waldemar@wdmsys.com>
2020-01-27 07:55:47 -0800

    db skeleton

--- persist.go
+++ persist.go
@@ -4,24 +4,73 @@ package main
 
 import (
 	bolt "go.etcd.io/bbolt"
+	"os"
+	"path"
 	"time"
 )
 
+/* Persistent store and restore
+
+The DB holds data for restoration on start up. The restoration is performed
+directly without locking. In contrast, storing data in DB is accomplished by
+sending v1 packets to DB channel.
+*/
+
 const (
-	dbpath   = "mapper.db"
-	eabucket = "ea-map"
+	dbname = "mapper.db"
+	oidbkt = "oid"
 )
 
-var db *bolt.DB
+var db *bolt.DB  // current DB
+var rdb *bolt.DB // restore DB
+var dbchan chan *PktBuf
 
-func open_db() {
+func db_listen() {
+
+	for pb := range dbchan {
+
+		retbuf <- pb
+	}
+}
+
+func stop_db() {
+
+	if db != nil {
+		db.Close()
+		db = nil
+	}
+	if rdb != nil {
+		rdb.Close()
+		rdb = nil
+	}
+}
+
+func start_db() {
 
 	var err error
 
-	log.info("opening DB: %v", dbpath)
+	dbpath := path.Join(cli.datadir, dbname)
+	rdbpath := dbpath + "~"
+
+	log.info("opening DB: %v", dbname)
+
+	if err := os.Rename(dbpath, rdbpath); err != nil {
+		if os.IsNotExist(err) {
+			rdb = nil
+		} else {
+			log.fatal("cannot rename %v: %v", dbname, err)
+		}
+	} else {
+		rdb, err = bolt.Open(rdbpath, 0666, &bolt.Options{Timeout: 1 * time.Second})
+		if err != nil {
+			log.fatal("cannot open %v: %v", rdbname, err)
+		}
+	}
+
 	db, err = bolt.Open(dbpath, 0666, &bolt.Options{Timeout: 1 * time.Second})
 	if err != nil {
-		db = nil
-		goexit <- "cannot open DB"
+		log.fatal("cannot create %v: %v", dbname, err)
 	}
+
+	go db_listen()
 }
