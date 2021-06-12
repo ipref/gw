af709be0ee0b5ae280e7a30c55430eab76ae03b7
waldemar <waldemar@wdmsys.com>
2021-06-12 16:49:16 -0700

    trigger recovery of expired eas

--- gen.go
+++ gen.go
@@ -5,6 +5,7 @@ package main
 import (
 	"crypto/rand"
 	rff "github.com/ipref/ref"
+	prng "math/rand" // where crypto/rand would be an overkill
 )
 
 /* Address and reference allocation
@@ -189,6 +190,112 @@ var gen_ea GenEA
 }
 */
 
+// trigger recovery of expired eas
+func (gen *GenEA) recover_expired_eas() {
+
+	const PAUSE = 257 // [ms]
+
+	recover_ea := make(chan *PktBuf, PKTQLEN)
+	pktid := uint16(prng.Intn(0xffff)) + 1 // make sure it's not zero
+
+	// initiate ea recovery scan
+
+	pb := <-getbuf
+	pb.write_v1_header(V1_REQ|V1_RECOVER_EA, pktid)
+	pkt := pb.pkt[pb.iphdr:]
+
+	off := V1_HDR_LEN
+
+	be.PutUint32(pkt[off+V1_OID:off+V1_OID+4], uint32(mapper_oid))
+	be.PutUint32(pkt[off+V1_MARK:off+V1_MARK+4], 0)
+
+	off += V1_MARK_LEN
+
+	be.PutUint32(pkt[off:off+4], 0) // find first address after 0.0.0.0
+
+	off += 4
+
+	pb.tail = pb.iphdr + off
+	be.PutUint16(pkt[V1_PKTLEN:V1_PKTLEN+2], uint16(off/4))
+	pb.peer = "recover_ea"
+	pb.schan = recover_ea
+	db.recv <- pb
+
+	// recover eas
+
+loop:
+	for pb = range recover_ea {
+
+		if err := pb.validate_v1_header(pb.len()); err != nil {
+
+			log.err("recover ea: invalid v1 packet from %v:  %v", pb.peer, err)
+			break loop
+		}
+
+		pkt := pb.pkt[pb.iphdr:pb.tail]
+
+		cmd := pkt[V1_CMD]
+
+		if cli.trace {
+			pb.pp_raw("recover ea:  ")
+		}
+
+		switch cmd {
+
+		case V1_ACK | V1_RECOVER_EA:
+
+			// pass the list to forwarders
+
+			pbf := <-getbuf
+			pbf.copy_from(pb)
+
+			pbf.pkt[pbf.iphdr+V1_CMD] = V1_DATA | V1_RECOVER_EA
+			pbf.peer = "recover_ea"
+			recv_tun <- pbf
+
+			// ask db for more
+
+			pkt = pb.pkt[pb.iphdr:pb.tail]
+			off = pb.len() - 4 // last returned ip address
+			if off < V1_HDR_LEN+V1_MARK_LEN {
+				break loop // no more eas
+			}
+
+			ip := be.Uint32(pkt[off : off+4])
+
+			off = V1_HDR_LEN + V1_MARK_LEN
+			be.PutUint32(pkt[off:off+4], ip+1) // next IP address
+
+			off += 4
+
+			pkt[V1_CMD] = V1_DATA | V1_RECOVER_EA
+
+			pktid = be.Uint16(pkt[V1_PKTID:V1_PKTID+2]) + 1
+			if pktid == 0 {
+				pktid++
+			}
+			be.PutUint16(pkt[V1_PKTID:V1_PKTID+2], pktid)
+
+			pb.tail = pb.iphdr + off
+			be.PutUint16(pkt[V1_PKTLEN:V1_PKTLEN+2], uint16(off/4))
+			pb.peer = "recover_ea"
+			pb.schan = recover_ea
+
+			sleep(PAUSE, PAUSE/4) // pause a little
+
+			db.recv <- pb
+
+		case V1_NACK | V1_RECOVER_EA:
+			break loop
+		default:
+			log.err("recover ea: invalid v1 pkt: %02x", cmd)
+			break loop
+		}
+	}
+
+	retbuf <- pb
+}
+
 // generate a random ea with second to last byte >= SECOND_BYTE
 func (gen *GenEA) next_ea() IP32 {
 
@@ -234,6 +341,8 @@ func (gen *GenEA) receive(pb *PktBuf) {
 
 func (gen *GenEA) start() {
 
+	// generate eas
+
 	go func(gen *GenEA) {
 		ea := gen.next_ea()
 		for {
@@ -244,6 +353,19 @@ func (gen *GenEA) start() {
 				gen.receive(pb)
 			}
 		}
+	}(gen)
+
+	// recover expired eas
+
+	go func(gen *GenEA) {
+
+		const DELAY = (MAPPER_TMOUT * 1000) / 3 // [ms]
+
+		for {
+			sleep(DELAY, DELAY/8)
+			gen.recover_expired_eas()
+		}
+
 	}(gen)
 }
 
